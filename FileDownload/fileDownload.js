ac6b12ae89e6b666e51147cf5de6edbb77f69d84
Raviteja Boddu <raviteja1312boddu@gmail.com>
2022-06-16 15:55:35 +0530

    Changes added to download image

--- FileDownload/fileDownload.js
+++ FileDownload/fileDownload.js
@@ -1,21 +1,14 @@
-function downloadFile(){
-    fetch("https://api.imgflip.com/get_memes")
-  .then((res) => res.json())
-  .then((data) => {
-        var memes = new Array();
-        memes = data.data.memes;
-        // console.log(memes);
-        // const randomNumber = Math.floor(Math.random() * memes.length)
-
-        const url = window.URL.createObjectURL(new Blob([memes[0]]));
-        // const url = memes[randomNumber].url; 
-        console.log(url)
-
-        
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download','image.jpg');
-        document.body.appendChild(link);
-        link.click();
-    })
+function downloadImage() {
+  axios({
+    url: "https://picsum.photos/200/300",
+    method: "GET",
+    responseType: "blob",
+  }).then((response) => {
+    const url = window.URL.createObjectURL(new Blob([response.data])); 
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "image.jpg");
+    document.body.appendChild(link);
+    link.click();
+  });
 }
