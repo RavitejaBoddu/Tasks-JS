2b86c4cd5c5dfffb82d33f0c625e7ea5f12b753d
Raviteja Boddu <raviteja1312boddu@gmail.com>
2022-06-16 13:12:09 +0530

    formatted the code

--- Callback/callback.js
+++ Callback/callback.js
@@ -1,15 +1,15 @@
-function getDate(){
-    const d = new Date();
-    var date = document.getElementById('date');
-    date.innerHTML = d.toLocaleDateString();
+function getDate() {
+  const d = new Date();
+  var date = document.getElementById("date");
+  date.innerHTML = d.toLocaleDateString();
 }
 
 getDate();
 
-function getCurrentTime(){
-    const date = new Date();
-    var time = document.getElementById('time');
-    time.innerHTML = date.toLocaleTimeString();
+function getCurrentTime() {
+  const date = new Date();
+  var time = document.getElementById("time");
+  time.innerHTML = date.toLocaleTimeString();
 }
 
 setInterval(getCurrentTime, 1000);
