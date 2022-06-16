8a55b663495ecedb2b389e032b406091b2fe18da
Raviteja Boddu <raviteja1312boddu@gmail.com>
2022-06-16 13:08:27 +0530

    Formatted the code

--- Factorial/factorial.js
+++ Factorial/factorial.js
@@ -1,23 +1,26 @@
-function getFactorial(){
-    var num = document.getElementById('fact-input').value;
+function getFactorial() {
+  var num = document.getElementById("fact-input").value;
 
-    var result = num;
-    if(num<0){
-        alert("Please enter positive integers");
-        return 0;
-    }else if(num==0 || num ==1){
-       return result = 1;
-    }
+  var result = num;
+  if (num < 0) {
+    alert("Please enter positive integers");
+    return 0;
+  } else if (num == 0 || num == 1) {
+    return (result = 1);
+  }
 
-    /* Using While loop */
-    // while(num>1)
-    // {
-    //     num--;
-    //     result = result * num;
-    // }
-    for(var i = num - 1; i >= 1; i--){
-        result = result * i ;
+  /* 
+    Using While loop 
+    while(num>1)
+    {
+        num--;
+        result = result * num;
     }
-    var res = document.getElementById('result');
-    res.innerHTML = result;
+    */
+
+  for (var i = num - 1; i >= 1; i--) {
+    result = result * i;
+  }
+  var res = document.getElementById("result");
+  res.innerHTML = result;
 }
