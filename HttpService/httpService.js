1f2dea79a7fb3a9921180866bb32979ca702e11e
Raviteja Boddu <raviteja1312boddu@gmail.com>
2022-06-16 14:59:35 +0530

    changes made to the table

--- HttpService/httpService.js
+++ HttpService/httpService.js
@@ -5,10 +5,22 @@ const xhr = new XMLHttpRequest();
 xhr.onload = function () {
   if (xhr.status === 200) {
     data = JSON.parse(xhr.responseText);
-    let table = document.querySelector("table");
-    let tabledata = Object.keys(data[0]);
-    generateTableHead(table, tabledata);
-    generateTable(table, data);
+
+    let tableData = "";
+    data.map((values) => {
+      tableData += `
+        <tr>
+            <td>${values.title}</td>
+            <td>${values.description}</td>
+            <td>${values.price} $</td>
+            <td><img src="${values.image} /></td>
+            <td>${values.rating.rate}</td>
+        </tr>
+      `;
+    });
+
+    let table = document.getElementById("tableBody");
+    table.innerHTML = tableData;
   } else if (xhr.status === 404) {
     console.log("No records found");
   }
@@ -27,34 +39,22 @@ xhr.send();
 // fetch("https://fakestoreapi.com/products")
 //   .then((res) => res.json())
 //   .then((data) => {
-//     console.log(data[0])
-//     let table = document.querySelector("table");
+// let tableData = "";
+// data.map((values) => {
+//   tableData += `
+//     <tr>
+//         <td>${values.title}</td>
+//         <td>${values.description}</td>
+//         <td>${values.price} $</td>
+//         <td><img src="${values.image} /></td>
+//         <td>${values.rating.rate}</td>
+//     </tr>
+//   `;
 
-//     let tdata = Object.keys(data[0]);
-//     generateTableHead(table, tdata);
-//     generateTable(table, data);
+// });
+
+// let table = document.getElementById("tableBody");
+// table.innerHTML = tableData;
 //   }).catch((err)=>{
 //     console.log(err);
 //   });
-
-function generateTableHead(table, data) {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key of data) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
-}
-
-function generateTable(table, data) {
-  for (let element of data) {
-    let row = table.insertRow();
-    for (key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      cell.appendChild(text);
-    }
-  }
-}
